d253ab22bb95fd0b764542aa9661e1ea1361c9ab
Kostya Serebryany <konstantin.s.serebryany@gmail.com>
2015-11-18 19:05:06 -0800

    Update build.sh

--- harfbuzz/build.sh
+++ harfbuzz/build.sh
@@ -8,12 +8,12 @@ rm -rf $NAME
 cp -rf harfbuzz $NAME
 cd $NAME
 ./autogen.sh
-FLAGS="-DHB_MAX_NESTING_LEVEL=3 -DHB_BUFFER_MAX_EXPANSION_FACTOR=3 -DHB_BUFFER_MAX_LEN_MIN=8 $SAN $COV -g"
+FLAGS=" $SAN $COV -g"
 CXX="clang++ $FLAGS"  CC="clang $FLAGS" CCLD="clang++ $FLAGS" ./configure --enable-static --disable-shared
 make -j
 )
 ln -sf $HOME/llvm/lib/Fuzzer .
 for f in Fuzzer/*cpp; do clang++ -std=c++11 -c $f -IFuzzer & done
 wait
-clang++ -g -std=c++11 harfbuzz/test/fuzzing/hb-fuzzer.cc $SAN $COV -I $NAME -I $NAME/src ./$NAME/src/.libs/libharfbuzz.a -lglib-2.0 Fuzzer*.o -o harfbuzz_${NAME}_fuzzer
+clang++ -g -std=c++11 harfbuzz/test/fuzzing/hb-fuzzer.cc $SAN $COV -I $NAME -I $NAME/src ./$NAME/src/.libs/libharfbuzz-fuzzing.a -lglib-2.0 Fuzzer*.o -o harfbuzz_${NAME}_fuzzer
 #clang++ -g -std=c++11 harfbuzz/test/fuzzing/hb-fuzzer.cc $SAN $COV -I $NAME -I $NAME/src ./$NAME/src/.libs/libharfbuzz.a -lglib-2.0 -DMAIN=main -o harfbuzz_${NAME}_run

8d36712b8b81a0948cf9424e5f012fb1ab76f73f
Kostya Serebryany <konstantin.s.serebryany@gmail.com>
2015-11-23 13:58:13 -0800

    sqlite: loop_body

--- sqlite/loop_body.sh
+++ sqlite/loop_body.sh
@@ -15,7 +15,7 @@ BUILD_SH=$P/build.sh
 SAN=-fsanitize=bool
 COV=-fsanitize-coverage=edge
 USE_COUNTERS=0
-# LIBFUZZER_EXTRA_FLAGS=-drill=1
+LIBFUZZER_EXTRA_FLAGS=-dict=afl-1.95b/testcases/_extras/sql.dict
 
 TARGET_NAME=sqlite
 

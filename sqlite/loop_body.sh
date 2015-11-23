9f3141c2f197096cf4dc5849241583e4a72746a0
Kostya Serebryany <konstantin.s.serebryany@gmail.com>
2015-11-23 13:45:03 -0800

    sqlite: loop_body

---
+++ sqlite/loop_body.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Copyright 2015 Google Inc. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+P=$(cd $(dirname $0) && pwd)
+COMMON=$P/../common
+
+MAX_LEN=10000
+MAX_TOTAL_TIME=3600
+BUCKET=gs://sql-fuzzing-corpora
+CORPUS=CORPORA/C1
+ARTIFACTS=CORPORA/ARTIFACTS
+BUILD_SH=$P/build.sh
+
+SAN=-fsanitize=bool
+COV=-fsanitize-coverage=edge
+USE_COUNTERS=0
+# LIBFUZZER_EXTRA_FLAGS=-drill=1
+
+TARGET_NAME=sqlite
+
+update_trunk() {
+  if [ -d sqlite ]; then
+    (cd sqlite && fossil update)
+  else
+    fossil clone https://www.sqlite.org/src sqlite.fossil
+    fossil open sqlite.fossil trunk
+  fi
+}
+
+DRY_RUN=0
+
+source $COMMON/loop_body.sh

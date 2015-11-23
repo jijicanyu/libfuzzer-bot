4cee1dbff707c846a336d4df2be9621a22a07f7b
Kostya Serebryany <konstantin.s.serebryany@gmail.com>
2015-11-23 14:46:47 -0800

    sqlite:

--- sqlite/build.sh
+++ sqlite/build.sh
@@ -16,6 +16,6 @@ for f in Fuzzer/*cpp; do clang++ -std=c++11 -c $f -IFuzzer & done
 wait
 ar ruv libFuzzer.a Fuzzer*.o
 rm -f Fuzzer*.o
-clang   -g -fno-omit-frame-pointer -I ${NAME}                 -O1 $SAN $COV -c $NAME/sqlite3.c                      -o sqlite3_$NAME.o
+clang   -g -fno-omit-frame-pointer -I ${NAME}                 -O2 $SAN $COV -c $NAME/sqlite3.c                      -o sqlite3_$NAME.o
 clang   -g -fno-omit-frame-pointer -I ${NAME} -DSQLITE_DEBUG  -O2 $SAN $COV -c libfuzzer-bot/sqlite/sqlite_fuzzer.c -o sqlite_fuzzer_$NAME.o
 clang++  -o sqlite_${NAME}_fuzzer $SAN $COV sqlite_fuzzer_$NAME.o sqlite3_$NAME.o libFuzzer.a

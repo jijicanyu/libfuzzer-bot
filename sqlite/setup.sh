a500e4b9f2cb78dc71dd0e921df19a5ca7edb5a0
Kostya Serebryany <konstantin.s.serebryany@gmail.com>
2015-11-23 14:02:05 -0800

    sqlite:

--- sqlite/setup.sh
+++ sqlite/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Copyright 2015 Google Inc. All Rights Reserved.
 # Licensed under the Apache License, Version 2.0 (the "License");
-sudo apt-get update && sudo apt-get install git subversion g++ cmake ninja-build apache2 screen fossil -y
+sudo apt-get update && sudo apt-get install git subversion g++ cmake ninja-build apache2 screen fossil tclsh tcl-dev -y
 git clone https://github.com/google/libfuzzer-bot.git
 svn co http://llvm.org/svn/llvm-project/llvm/trunk/lib/Fuzzer
+wget http://lcamtuf.coredump.cx/afl/releases/afl-latest.tgz

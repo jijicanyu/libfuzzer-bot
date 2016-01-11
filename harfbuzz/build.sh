0147ded2b91574d98f3e64912f355aa06a202ef4
Kostya Serebryany <konstantin.s.serebryany@gmail.com>
2016-01-11 11:27:38 -0800

    harfbuzz update

--- harfbuzz/build.sh
+++ harfbuzz/build.sh
@@ -12,7 +12,6 @@ FLAGS=" $SAN $COV -g"
 CXX="clang++ $FLAGS"  CC="clang $FLAGS" CCLD="clang++ $FLAGS" ./configure --enable-static --disable-shared
 make -j -C src fuzzing
 )
-ln -sf $HOME/llvm/lib/Fuzzer .
 for f in Fuzzer/*cpp; do clang++ -std=c++11 -c $f -IFuzzer & done
 wait
 clang++ -g -std=c++11 harfbuzz/test/fuzzing/hb-fuzzer.cc $SAN $COV -I $NAME -I $NAME/src ./$NAME/src/.libs/libharfbuzz-fuzzing.a -lglib-2.0 Fuzzer*.o -o harfbuzz_${NAME}_fuzzer

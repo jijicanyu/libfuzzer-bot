0a12427f0bdc180e550c6ed733f49dd4a1fbf591
Mike Aizatsky <aizatsky@google.com>
2016-01-15 18:05:18 -0800

    moving build env into base

--- dockerfiles/base-clang/build_clang.sh
+++ dockerfiles/base-clang/build_clang.sh
@@ -6,7 +6,10 @@ cd /work/llvm
 
 if [ ! -f CMakeCache.txt ]
 then
-    cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release /src/llvm
+    cmake -G "Ninja" \
+      -DCMAKE_C_COMPILER=/usr/bin/gcc \
+      -DCMAKE_CXX_COMPILER=/usr/bin/g++ \
+      -DCMAKE_BUILD_TYPE=Release /src/llvm
 fi
 
 ninja

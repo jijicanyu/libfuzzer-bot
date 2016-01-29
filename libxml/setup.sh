d2fe44be67c4028aaccfd01fad4ba84c756734eb
Kostya Serebryany <konstantin.s.serebryany@gmail.com>
2016-01-28 18:23:33 -0800

    libxml

--- libxml/setup.sh
+++ libxml/setup.sh
@@ -1,5 +1,5 @@
 !/bin/bash
 # Copyright 2015 Google Inc. All Rights Reserved.
 # Licensed under the Apache License, Version 2.0 (the "License");
-sudo apt-get update && sudo apt-get install git subversion g++ cmake ninja-build apache2 screen  autoconf libtool python-dev -y
+sudo apt-get update && sudo apt-get install git subversion g++ cmake ninja-build apache2 screen  autoconf libtool python-dev pkg-config -y
 git clone https://github.com/google/libfuzzer-bot.git

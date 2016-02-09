f0500c482ca2650ccdf7cb587ef350a972316b7b
Mike Aizatsky <aizatsky@google.com>
2016-02-09 11:47:37 -0800

    removing ONBUILD instruction - makes builds non-cacheable.

--- dockerfiles/all.sh
+++ dockerfiles/all.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e -x
 
-docker build -t libfuzzer/base base/
-docker build -t libfuzzer/base-clang base-clang/
-docker build -t libfuzzer/base-fuzzer base-fuzzer/
+docker build $* -t libfuzzer/base base/
+docker build $* -t libfuzzer/base-clang base-clang/
+docker build $* -t libfuzzer/base-fuzzer base-fuzzer/
 
-docker build -t libfuzzer/pcre2 pcre2/
-docker build -t libfuzzer/boringssl boringssl/
-docker build -t libfuzzer/freetype2 freetype2/
+docker build $* -t libfuzzer/libxml libxml/
+docker build $* -t libfuzzer/pcre2 pcre2/
+docker build $* -t libfuzzer/boringssl boringssl/
+docker build $* -t libfuzzer/freetype2 freetype2/

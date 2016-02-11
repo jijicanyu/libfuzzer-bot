3493430af73f4c4b2234b279b4d6457426b51f8f
Kostya Serebryany <konstantin.s.serebryany@gmail.com>
2016-02-10 21:07:02 -0800

    Update loop_body.sh

--- re2/loop_body.sh
+++ re2/loop_body.sh
@@ -5,7 +5,7 @@ P=$(cd $(dirname $0) && pwd)
 COMMON=$P/../common
 
 MAX_LEN=16
-MAX_TOTAL_TIME=30
+MAX_TOTAL_TIME=7200
 BUCKET=gs://re-fuzzing-corpora
 CORPUS=CORPORA/C1
 ARTIFACTS=CORPORA/ARTIFACTS

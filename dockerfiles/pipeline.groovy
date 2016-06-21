c2c2be0cb67d3d6ca83acf6c65d7a30ed2513553
Mike Aizatsky <aizatsky@google.com>
2016-06-21 14:18:57 -0700

    testing reusable pipeline function

--- dockerfiles/pipeline.groovy
+++ dockerfiles/pipeline.groovy
@@ -8,25 +8,51 @@ def call(body) {
     // Mandatory configuration
     def dockerfile = config["dockerfile"]
     assert dockerfile : "dockerfile should be specified"
+    def gitUrl = config["git"]
+    assert gitUrl : "git should be specified"
 
     // Optional configuration
     def projectName = config["name"] ?: env.JOB_BASE_NAME
     def sanitizers = config["sanitizers"] ?: ["address", "memory"]
 
-    node {
-      echo "Config: ${config}"
+    def date = java.time.format.DateTimeFormatter.ofPattern("yyyyMMddHHmm").format(java.time.LocalDateTime.now())
 
-      echo "Building project ${projectName} with Dockerfile=${dockerfile}"
+    node {
+      echo "Building project $projectName with Dockerfile=$dockerfile"
 
       for (int i = 0; i < sanitizers.size(); i++) {
         def sanitizer = sanitizers[i]
-        def docker_tag = "${projectName}-${sanitizer}"
+        def dockerTag = "$projectName-$sanitizer"
 
         dir(sanitizer) {
-          echo "*** Building with ${sanitizer} sanitizer"
+          echo "*** Building with $sanitizer sanitizer"
+
+          // See JENKINS-33511
+          sh 'pwd > pwd.current'
+          def workspace = readFile('pwd.current').trim()
+          def out = "$workspace/out"
+
+          dir('libfuzzer-bot') {
+              git url: 'https://github.com/google/libfuzzer-bot/'
+          }
+          dir(projectName) {
+              git url: gitUrl
+          }
+
+          sh "docker build -t libfuzzer/$dockerTag -f $dockerfile ."
+
+          sh "rm -rf $out"
+          def zipFile= "$dockerTag-$date.zip"
+
+          sh "mkdir -p $out"
+          sh "docker run -v $workspace:/workspace -v $out:/out -e sanitizer_flags=\"-fsanitize=$sanitizer\" -t libfuzzer/$dockerTag"
+          sh "zip -j $zipFile $out/*"
+          sh "gsutil cp $zipFile gs://clusterfuzz-builds/$projectName/"
         }
       }
     }
+
+  echo 'Done'
 }
 
 return this;

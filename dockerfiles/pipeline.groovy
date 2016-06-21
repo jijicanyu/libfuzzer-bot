5196f65e3cc1f66eb742b98cf4c1a7aadf8e5d71
Mike Aizatsky <aizatsky@google.com>
2016-06-21 14:06:55 -0700

    testing reusable pipeline function

--- dockerfiles/pipeline.groovy
+++ dockerfiles/pipeline.groovy
@@ -5,8 +5,13 @@ def call(body) {
     body.delegate = config
     body()
 
+    def projectName = config.get("name", env.JOB_BASE_NAME)
+    def dockerfile = config.get("dockerfile")
+
     node {
       echo "Config: ${config}"
+
+      echo "Building project ${projectName} with Dockerfile=${dockerfile}"
     }
 }
 

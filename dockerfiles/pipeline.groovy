32e9c26b5fe8b10d171eb07f163a8d8fb101848d
Mike Aizatsky <aizatsky@google.com>
2016-06-21 14:13:03 -0700

    testing reusable pipeline function

--- dockerfiles/pipeline.groovy
+++ dockerfiles/pipeline.groovy
@@ -1,3 +1,5 @@
+def SANITIZERS = ["address", "memory"]
+
 def call(body) {
     // evaluate the body block, and collect configuration into the object
     def config = [:]
@@ -5,15 +7,27 @@ def call(body) {
     body.delegate = config
     body()
 
-    def projectName = config["name"] ?: env.JOB_BASE_NAME
+    // Mandatory configuration
     def dockerfile = config["dockerfile"]
-
     assert dockerfile : "dockerfile should be specified"
+
+    // Optional configuration
+    def projectName = config["name"] ?: env.JOB_BASE_NAME
+    def sanitizers = config["sanitizers"] ?: SANITIZERS
 
     node {
       echo "Config: ${config}"
 
       echo "Building project ${projectName} with Dockerfile=${dockerfile}"
+
+      for (int i = 0; i < SANITIZERS.size(); i++) {
+        def sanitizer = SANITIZERS[i]
+        def docker_tag = "${projectName}-${sanitizer}"
+
+        dir(sanitizer) {
+          echo "*** Building with ${sanitizer} sanitizer"
+        }
+      }
     }
 }
 
